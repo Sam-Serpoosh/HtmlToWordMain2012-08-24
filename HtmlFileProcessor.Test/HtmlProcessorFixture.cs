8f00d72ed6e6431056cc0c271268e1046297add2
Sam Serpoosh <ssjesus287@gmail.com>
2012-08-29 00:37:10 -0400

    ignoring between openning and closing angle brackets for html processing

---
+++ HtmlFileProcessor.Test/HtmlProcessorFixture.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace HtmlFileProcessor.Test
+{
+    [TestFixture]
+    public class HtmlProcessorFixture
+    {
+        [Test]
+        public void IgnoresFromOpenningAngleBracketToClosingOne()
+        {
+            var controlName = "MMSControl";
+            var htmlText = "<span class=\"identifier\">" + controlName + "</span>";
+            var htmlProcessor = new HtmlProcessor();
+            var value = htmlProcessor.ExtractValueOut(htmlText);
+            Assert.AreEqual(controlName, value);
+        }
+    }
+}
---
+++ HtmlFileProcessor/HtmlProcessor.cs
@@ -0,0 +1,22 @@
+﻿namespace HtmlFileProcessor
+{
+    public class HtmlProcessor
+    {
+        public string ExtractValueOut(string htmlText)
+        {
+        	var extractedText = string.Empty;
+        	var shouldBeAdded = true;
+			foreach (var ch in htmlText)
+			{
+				if (ch.Equals('<'))
+					shouldBeAdded = false;
+				if (ch.Equals('>'))
+					shouldBeAdded = true;
+				if (shouldBeAdded && !ch.Equals('>'))
+					extractedText += ch.ToString();
+			}
+
+        	return extractedText;
+        }
+    }
+}
